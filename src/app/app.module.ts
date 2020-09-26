23c2696b5d7e48c96213381c1860c29ae270514e
jonarivado <jonathan.dotzler@gmail.com>
2020-09-26 11:59:56 +0200

    refined landing page & added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "../components/footer/footer.component";
+
+@NgModule({
+    declarations: [FooterComponent],
+    exports: [FooterComponent]
+})
+export class ComponentsModule{}
