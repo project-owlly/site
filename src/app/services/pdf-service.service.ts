9cf27fda5cf9490e74f222d4259d4f50283a23df
Sandro Scalco <sandro.scalco@liitu.ch>
2020-10-18 13:14:24 +0200

    mockdata

--- src/app/services/pdf-service.service.ts
+++ src/app/services/pdf-service.service.ts
@@ -29,10 +29,18 @@ export class PdfServiceService {
         birth_date: '03.06.1988',
         locality: 'Schaffhausen',
         postal_code: '8200',
+        canton: 'ZH',
         street_address: 'Villenstrasse 4',
-        verified_simple: {given_name: true, family_name: true, birth_date: true, locality: false, postal_code: false, street_address: false},
+        verified_simple: {
+          given_name: true,
+          family_name: true,
+          birth_date: true,
+          locality: false,
+          postal_code: false,
+          street_address: false,
+        },
       },
-      owllyId: '123',
+      owllyId: 'vrrYZoolx2XSy23RW63f',
     });
     // TODO: unsubscribe or first
     obs.subscribe((res) => {
