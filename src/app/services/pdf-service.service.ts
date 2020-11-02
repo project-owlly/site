ec132c76bd1663d4daacafebe54f6880c9dd0d06
Sandro Scalco <sandro.scalco@liitu.ch>
2020-11-02 18:19:13 +0100

    post

--- src/app/services/pdf-service.service.ts
+++ src/app/services/pdf-service.service.ts
@@ -25,7 +25,10 @@ export class PdfServiceService {
           ' Velounfälle verhindern  Seit 2011 haben sich die Velounfälle mehr als verdoppelt! Autofreie Velorouten entflechten Auto- und Veloverkehr und schaffen so mehr Sicherheit für alle. Schluss mit Stillstand  Seit Jahren herrscht in der Stadt Zürich in Sachen Velo Stillstand. Mit der Velorouten-Initiative sorgen wir dafür, dass die Stadt einen Gang hochschaltet und wir endlich vorwärts kommen! Sicher auf dem Velo und zu Fuss  Die Velorouten-Initiative macht schluss mit den unsäglichen «Mischverkehrsflächen», die für Fussgänger/-innen gefährlich sind. Sichere Velowege auf den Strassen bedeuten gleichzeitig, dass die Trottoirs endlich wieder nur für Fussgänger/-innen sind und sich alle sicher fühlen können. Ein Beitrag zum Klimaschutz  Nur wer sich auf dem Velo sicher fühlt, nimmt statt dem Auto auch mal schnell das Velo. Sichere Velorouten leisten also auch einen wichtigen Beitrag zur Erreichung unserer Klimaziele.',
         title: 'Velorouten-Initiative',
         type: 'initiative',
-        published: new Date(),
+        published: new Date().toISOString(),
+        author: 'urheber der initiative',
+        ruleName: 'canton',
+        ruleValue: 'sh',
       },
       userData: {
         sub: '0x8870e311Fa7C3B75934C443d3aD56262812B3f8D',
@@ -44,8 +47,13 @@ export class PdfServiceService {
     // const obs = callable({ coolMsg: this.myInput });
     const obs = callable(data);
     // TODO: unsubscribe or first
-    obs.subscribe((res) => {
-      this.pdf = res.result;
-    });
+    obs.subscribe(
+      (res) => {
+        this.pdf = res.result;
+      },
+      (error) => {
+        console.log(JSON.stringify(error));
+      }
+    );
   }
 }
