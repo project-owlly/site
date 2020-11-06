ade657beecb36ef43f772f50a20d1057d1771440
Sandro Scalco <sandro.scalco@liitu.ch>
2020-11-06 16:40:01 +0100

    fix module

--- src/app/pages/return/return.module.ts
+++ src/app/pages/return/return.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { ReturnPageRoutingModule } from './return-routing.module';
+import {ReturnPageRoutingModule} from './return-routing.module';
 
-import { ReturnPage } from './return.page';
+import {ReturnPage} from './return.page';
+
+import {ComponentsModule} from 'src/app/components/core/components.module';
+import {SignatureModule} from '../../components/signature/signature.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ReturnPageRoutingModule
-  ],
-  declarations: [ReturnPage]
+  imports: [CommonModule, FormsModule, IonicModule, ReturnPageRoutingModule, ComponentsModule, SignatureModule],
+  declarations: [ReturnPage],
 })
 export class ReturnPageModule {}
