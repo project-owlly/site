9a66ef787f5cea16d913180af012a5e2993991a9
jonarivado <jonathan.dotzler@gmail.com>
2020-11-12 22:38:24 +0100

    interface for initiative data & send as object

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -2,17 +2,31 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {IonSlides, IonSelect, IonSelectOption} from '@ionic/angular'
 import { promise } from 'selenium-webdriver';
 import { resolve } from 'url';
-//import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+
+import { createData } from "./createInterface";
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
   styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
+
+createData: createData = {
+  text: '',
+  title: '',
+  type: '',
+  published: '',
+  author: '',
+  ruleValue: '',
+  goals: ['']
+
+} 
 //Variables to read user input  
 ebene;
 begehren;
- // createForm: FormGroup;
+//form init
+createForm: FormGroup;
 
   @ViewChild('mySlider') slides: IonSlides
 
@@ -39,16 +53,32 @@ begehren;
   }
   
 
-  constructor(/*public fb: FormBuilder*/) { }
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
-    /*
+    //TODO: send every goal, not only last one
     this.createForm = this.fb.group({
-      level: ['', [Validators.required]],
-      
+      text: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      published: [''],
+      author: [''],
+      ruleValue: ['', [Validators.required]],
+      goals: [''],
     });
-    */
+    
   }
 
+  sendInitiative() {
+    this.createData.text = this.createForm.value.text;
+    this.createData.title = this.createForm.value.title;
+    this.createData.type = this.createForm.value.type;
+    this.createData.published = this.createForm.value.published;
+    this.createData.author = this.createForm.value.author;
+    this.createData.ruleValue = this.createForm.value.ruleValue;
+    this.createData.goals = this.createForm.value.goals;
+
+    console.log(this.createData);
+  }
   
 }
