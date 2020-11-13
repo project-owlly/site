46d0c351320e39c69e114b134c8fae881d94266c
peterpeterparker <david.dalbusco@outlook.com>
2020-11-13 13:28:10 +0100

    refactor: move login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    loadChildren: () => import('./pages/auth/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'start',
