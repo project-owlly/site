49cf2544375b445aa0a6e85708abc4db101b557a
peterpeterparker <david.dalbusco@outlook.com>
2020-11-20 12:39:58 +0100

    refactor: todo and no query

--- src/app/pages/wizard/start/start.guard.ts
+++ src/app/pages/wizard/start/start.guard.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 
 import {Observable} from 'rxjs';
-import {map, first} from 'rxjs/operators';
 
 import {OwllyService} from 'src/app/services/owlly.service';
 
@@ -14,12 +13,10 @@ export class StartGuard implements CanActivate {
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     // Technically we would be able to check here if the owllyId exists in DB and if not point to home or somewhere else.
+    // TODO: but should we do that? For performance and cost reason I would say no, at least not yet.
     if (!next.params.owllyId || next.params.owllyId === '') {
       return this.router.parseUrl('/');
     } else {
-      //TODO: do a data check :)
-      let owlly = this.owlly.owlly(next.params.owllyId);
-
       return true;
     }
   }
